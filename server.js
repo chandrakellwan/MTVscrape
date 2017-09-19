3dff2580eaedce9e49da9cda36843d07414dc019
Chandra Kellwan <Chandra@Chandras-MacBook-Pro.local>
2017-09-18 20:42:17 -0400

    uncommented out localhost

--- server.js
+++ server.js
@@ -39,7 +39,7 @@ app.engine("handlebars", exphbs({ defaultLayout: "main" }));
 app.set("view engine", "handlebars");
 
 //mongoose localhost connection
-//mongoose.connect("mongodb://localhost/mtvScrape");
+mongoose.connect("mongodb://localhost/mtvScrape");
 
 mongoose.connect("mongodb://heroku_36qch87t:6e9t74vm4jsdcnbsj0kefeg5h9@ds141514.mlab.com:41514/heroku_36qch87t")
 var db = mongoose.connection;
